790e7e199b014195d161fae0de840712b12bfbed
Rajkumar-pepcus <102852203+Rajkumar-pepcus@users.noreply.github.com>
2022-04-05 12:17:35 +0530

    Add files via upload

---
+++ crudapi-1/src/main/java/com/pepcus/apicrud/model/Student.java
@@ -0,0 +1,56 @@
+package com.pepcus.apicrud.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+  private long id;
+	@Column(name="name",nullable=false)
+  private String name;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneno() {
+		return phoneno;
+	}
+	public void setPhoneno(String phoneno) {
+		this.phoneno = phoneno;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Column(name="email")
+  private String email;
+	@Column(name="phoneno")
+  private String phoneno;
+	@Column(name="address")
+  private String address;
+  
+  
+}
